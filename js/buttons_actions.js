ddcb809776ea68d4e57465ee97a694d252150dbc
Diego Camarmas Alonso <diego.camarmas.alonso@gmail.com>
2018-11-21 15:05:01 +0100

    1.1

--- js/buttons_actions.js
+++ js/buttons_actions.js
@@ -1,21 +1,62 @@
+/*Botones menu de configuracion*/
+$("#load_arch_default").click(function(){
+  $("#available_arch").show();
+  $("#load_menu_arch").hide();
+  $("#save_menu_arch").hide();
+  $("#new_components").hide();
+});
+
+$("#load_arch").click(function(){
+  $("#available_arch").hide();
+  $("#load_menu_arch").show();
+  $("#save_menu_arch").hide();
+  $("#new_components").hide();
+});
+
+$("#save_arch").click(function(){
+  $("#available_arch").hide();
+  $("#load_menu_arch").hide();
+  $("#save_menu_arch").show();
+  $("#new_components").hide();
+});
+
+$("#new_arch").click(function(){
+  $("#available_arch").hide();
+  $("#load_menu_arch").hide();
+  $("#save_menu_arch").hide();
+  $("#new_components").show();
+});
+
+/*Botones modos*/
 $(".micro_btn").click(function(){
   $("#simulator").hide();
   $("#microcode").show();
   $("#assembly").hide();
+  $("#architecture_menu").hide();
 });
 
 $(".assembly_btn").click(function(){
   $("#simulator").hide();
   $("#microcode").hide();
   $("#assembly").show();
+  $("#architecture_menu").hide();
 });
 
 $(".simulator_btn").click(function(){
   $("#simulator").show();
   $("#microcode").hide();
   $("#assembly").hide();
+  $("#architecture_menu").hide();
 });
 
+$(".arch_btn").click(function(){
+  $("#simulator").hide();
+  $("#microcode").hide();
+  $("#assembly").hide();
+  $("#architecture_menu").show();
+});
+
+/*Selector de modo de datos*/
 $("#selectData").change(function(){
   var value = document.getElementById("selectData").value;
   if(value == "CPU-FP Registers"){
