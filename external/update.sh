3e3f27f33d91b6b8a280a45dcad12bb85eb19a16
Diego Camarmas Alonso <diego.camarmas.alonso@gmail.com>
2018-12-11 17:24:56 +0100

    1.1.8

---
+++ external/update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+
+mv -f *.js /tmp/
+wget https://unpkg.com/bootstrap/dist/css/bootstrap.min.css
+wget https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.css
+wget https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css
+wget https://unpkg.com/vue
+wget https://unpkg.com/babel-polyfill@latest/dist/polyfill.min.js
+wget https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.js
+wget https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+wget https://peterolson.github.io/BigInteger.js/BigInteger.min.js
+
