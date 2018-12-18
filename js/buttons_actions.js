5e0d42548c25230a4827aaaafcf0ff8c48ced721
Diego Camarmas Alonso <diego.camarmas.alonso@gmail.com>
2018-12-19 00:26:00 +0100

    1.1.12

--- js/buttons_actions.js
+++ js/buttons_actions.js
@@ -17,50 +17,21 @@ $("#view_arch").click(function(){
   $("#view_components").show();
 });
 
-/*Botones menu de instrucciones*/
-$("#load_assDef").click(function(){
-  $("#load_instructions_set").show();
-  $("#save_menu_assDef").hide();
-  $("#new_instruction_set").hide();
-});
-
-$("#save_assDef").click(function(){
-  $("#load_instructions_set").hide();
-  $("#save_menu_assDef").show();
-  $("#new_instruction_set").hide();
-});
-
-$("#new_assDef").click(function(){
-  $("#load_instructions_set").hide();
-  $("#save_menu_assDef").hide();
-  $("#new_instruction_set").show();
-});
-
 /*Botones modos*/
-$(".assDef_btn").click(function(){
-  $("#simulator").hide();
-  $("#assDefinition").show();
-  $("#assembly").hide();
-  $("#architecture_menu").hide();
-});
-
 $(".assembly_btn").click(function(){
   $("#simulator").hide();
-  $("#assDefinition").hide();
   $("#assembly").show();
   $("#architecture_menu").hide();
 });
 
 $(".simulator_btn").click(function(){
   $("#simulator").show();
-  $("#assDefinition").hide();
   $("#assembly").hide();
   $("#architecture_menu").hide();
 });
 
 $(".arch_btn").click(function(){
   $("#simulator").hide();
-  $("#assDefinition").hide();
   $("#assembly").hide();
   $("#architecture_menu").show();
 });
