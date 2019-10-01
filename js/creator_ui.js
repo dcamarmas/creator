041ceaff37f60e0b4881af6ce1082dc483864482
Alejandro <acaldero@inf.uc3m.es>
2019-10-01 18:15:54 +0200

    1.3: minor ui improvements

--- js/creator_ui.js
+++ js/creator_ui.js
@@ -20,6 +20,7 @@
 
 
   function show_notification ( msg, type ){
+    // show notification
     app._data.alertMessage = msg ;
     app._data.type         = type ;
     app.$bvToast.toast(app._data.alertMessage, {
@@ -29,11 +30,36 @@
       autoHideDelay: app._data.notificationTime,
     });
 
+    // add notification to the notification summary
     var date = new Date();
     notifications.push({ mess: app._data.alertMessage, 
                          color: app._data.type, 
                          time: date.getHours()+":"+date.getMinutes()+":"+date.getSeconds(), 
                          date: date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear() }); 
     return true ;
+  }
+
+  function show_loading(){
+    // if loading is programmed, skip
+    if(toHandler != null){
+      return;
+    }
+
+    // after half second show the loading spinner 
+    toHandler = setTimeout(function(){
+      $(".loading").show();
+      toHandler = null;
+    }, 500);
+  }
+
+  function hide_loading(){
+    // if loading is programmed, cancel it
+    if(toHandler != null){
+      clearTimeout(toHandler);
+      toHandler = null;
+    }
+
+    // disable loading spinner
+    $(".loading").hide();
   }
 
