c420530f534432a106419d4b788a745bcb1ea324
dcamarmas <diego.camarmas.alonso@gmail.com>
2019-10-04 23:59:42 +0200

    1.3-cmf

--- mk_min.sh
+++ mk_min.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -x
 
-cat js/creator_ui.js \
+cat js/creator_compiler.js \
+    node/creator_node.js > node/min.creator_node.js
+
+cat js/creator_compiler.js \
+    js/creator_ui.js \
     js/app.js > js/min.creator_web.js
 
