a0cf568e65db65a4c8f2ab905e6612d0404ab015
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2020-06-25 21:28:07 +0200

    1.4

--- mk_min.sh
+++ mk_min.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -x
 
-cat js/creator_compiler.js \
-    js/creator_executor.js \
-    node/creator_node.js > node/min.creator_node.js
-
-cat js/creator_compiler.js \
-    js/creator_executor.js \
-    js/creator_ui.js \
+cat js/creator_ui.js \
     js/app.js > js/min.creator_web.js
 
