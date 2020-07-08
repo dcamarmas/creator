2016e9e1fb04c9149d64cc9466120de3fcd62462
Alejandro <acaldero@inf.uc3m.es>
2020-07-08 20:02:22 +0200

    1.4: initial creator_compiler.js

--- mk_min.sh
+++ mk_min.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-set -x
 
-cat js/creator_ui.js \
+# welcome
+echo ""
+echo "  CREATOR packer"
+echo " ----------------"
+echo ""
+echo "  Requirements:"
+echo "  * apt-get install terser"
+echo ""
+if [ $# -gt 0 ]; then
+     set -x
+fi
+
+
+# skeleton
+echo "  Packing:"
+echo "  * min.creator_web.js..."
+cat js/creator_compiler.js \
+    js/creator_ui.js \
     js/creator_preload.js \
-    js/app.js > js/min.creator_web.js
+    js/app.js > js/creator_web.js
+terser -o js/min.creator_web.js js/creator_web.js
+rm -fr js/creator_web.js
+
+
+# the end
+echo ""
+echo "  CREATOR packed (if no error was shown)."
 
