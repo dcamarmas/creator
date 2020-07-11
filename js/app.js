08a5813fec1a8c131481a563de2334fdf3709da9
luck5941 <lucaselvira96@gmail.com>
2020-07-11 12:22:47 +0200

    Fix the bug on the regex that not allow to keep similar names on diferents components's registers. The problem was the way that the groups interact. Not tested on production yet

--- js/app.js
+++ js/app.js
@@ -3943,17 +3943,17 @@ try{
               }
               for (var j = architecture.components[i].elements.length-1; j >= 0; j--){
                 var re;
-                let myMatch;
                 /*Write in the register*/
 
                 /*TODO: Conflicto RISC-V*/
-                /*re = new RegExp( "(?:\\W|^)(" + architecture.components[i].elements[j].name+" *=[^=])", "g");
+                re = new RegExp( "(?:\\W|^)((" + architecture.components[i].elements[j].name+") *=[^=])", "g");
+                let myMatch;
                 while ((myMatch = re.exec(auxDef)) != null) {
-                    auxDef = auxDef.replace(myMatch[1], `reg${regIndex}=`)
+                    auxDef = auxDef.replace(myMatch[2], `reg${regIndex}=`)
                     auxDef = `var reg${regIndex}=null\n${auxDef}\nthis.writeRegister(reg${regIndex}, ${i}, ${j});`;
                     myMatch.index=0;
-                }*/
-
+                }
+                /*
                 re = new RegExp(architecture.components[i].elements[j].name+" *=[^=]");
                 if (auxDef.search(re) != -1){
                   re = new RegExp(architecture.components[i].elements[j].name+" *=","g");
@@ -3963,7 +3963,7 @@ try{
                   auxDef = auxDef + "\n this.writeRegister(reg"+regIndex+","+i+" ,"+j+");"
                   regIndex++;
                 }
-
+                */
 
                 if(architecture.components[i].type == "integer"){
                   re = new RegExp("R"+regNum+" *=[^=]");
