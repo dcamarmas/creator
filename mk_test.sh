f4ec05de91b77bf923224a91536ff93a5c8b5083
Diego <diego.camarmas.alonso@gmail.com>
2020-07-15 19:43:07 +0200

    2.0B2: mk_test update

--- mk_test.sh
+++ mk_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 
-for I in $(seq 1 1 11)
+for I in $(seq 1 1 8)
 do
   echo "Testing ./examples/MIPS/example$I..."
   ./creator.sh -a ./architecture/MIPS-32-like.json -s ./examples/MIPS/example$I.txt
