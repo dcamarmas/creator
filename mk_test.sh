1bb97741d0a3c5d7cd7ab69be3552084c5298c9a
Diego <diego.camarmas.alonso@gmail.com>
2020-07-15 21:39:48 +0200

    2.0B2: creator.sh updated

--- mk_test.sh
+++ mk_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #set -x
 
+npm install
+
 for I in $(seq 1 1 8)
 do
   echo "Testing ./examples/MIPS/example$I..."
   ./creator.sh -a ./architecture/MIPS-32-like.json -s ./examples/MIPS/example$I.txt
   echo ""
 done
-
