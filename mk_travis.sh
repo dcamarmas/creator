09574fda19220ee35e32f9012354392d0d144e1b
Diego <diego.camarmas.alonso@gmail.com>
2020-07-22 13:51:33 +0200

    2.0.2: travis repository improved

--- mk_travis.sh
+++ mk_travis.sh
@@ -9,20 +9,20 @@ echo " MIPS: examples"
 MIPS_TEST="2 3 4 5 6 7 8 11"
 for I in $MIPS_TEST;
 do
-   echo -n " * ./examples/MIPS/example$I: "
+   echo -n " * ./travis/MIPS/correct/example$I: "
    ./creator.sh -a ./architecture/MIPS-32-like.json \
-                -s ./examples/MIPS/example$I.txt \
-                -r ./examples/MIPS/example$I.output --quiet
+                -s ./travis/MIPS/correct/example$I.txt \
+                -r ./travis/MIPS/correct/example$I.output --quiet
 done
 
 echo " MIPS: common errors"
 MIPS_TEST="1 2"
 for I in $MIPS_TEST;
 do
-   echo " * ./examples/MIPS/error$I: "
+   echo " * ./travis/MIPS/error/error$I: "
    ./creator.sh -a ./architecture/MIPS-32-like.json \
-                -s ./examples/MIPS/error$I.txt \
-                -r ./examples/MIPS/error$I.output --quiet
+                -s ./travis/MIPS/error/error$I.txt \
+                -r ./travis/MIPS/error/error$I.output --quiet
 done
 
 
@@ -34,9 +34,9 @@ echo " RISC-V:"
 RV_TEST="1"
 for I in $RV_TEST;
 do
-  echo -n " * ./examples/riscv/example$I: "
+  echo -n " * ./travis/riscv/correct/example$I: "
   ./creator.sh -a ./architecture/RISC-V-like.json \
-               -s ./examples/riscv/example$I.txt \
-               -r ./examples/riscv/example$I.output --quiet
+               -s ./travis/riscv/correct/example$I.txt \
+               -r ./travis/riscv/correct/example$I.output --quiet
 done
 
