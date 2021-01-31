c30bbf4bb8131328b6cada1dc51f75896c6f8547
Diego <diego.camarmas.alonso@gmail.com>
2021-01-31 19:37:45 +0100

    3.0: more components added

---
+++ components/simulator/creator_uielto_memory_stack.js
@@ -0,0 +1,194 @@
+/*
+ *  Copyright 2018-2021 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos
+ *
+ *  This file is part of CREATOR.
+ *
+ *  CREATOR is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CREATOR is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with CREATOR.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+        /* jshint esversion: 6 */
+
+        var uielto_memory_stack = {
+
+			  props:      {
+											unallocated_memory:   { type: Array, required: true },
+											memory:   { type: Array, required: true }
+										},
+
+				data: 			function () {
+											return {
+          							/*Memory table fields*/
+      									memFields: ['Tag', 'Address', 'Binary', 'Value']
+  										}
+        						},
+
+			  methods: 		{
+			  							select_stack_type(record, index){
+								        app._data.row_index = index;  //TODO: vue bidirectional updates
+								        app.$refs['stack_modal'].show(); //TODO: vue bidirectional updates
+								      }				
+			  						},
+
+      	template:   '	<div class="col-lg-12 col-sm-12">' +
+										'	  <b-table sticky-header ' +
+										'	           striped ' +
+										'	           small ' +
+										'	           hover ' +
+										'	           :items="unallocated_memory.concat(Object.values(memory)[2])" ' +
+										'	           :fields="memFields" ' +
+										'	           class="memory_table" ' +
+										'	           @row-clicked="select_stack_type">' +
+										'	' +
+										'	    <template v-slot:head(Tag)="row">' +
+										'	      &nbsp;' +
+										'	    </template>' +
+										'	' +
+										'	    <template v-slot:cell(Tag)="row">' +
+										'	      <div v-for="item in architecture_hash">' +
+										'	        <div v-for="item2 in architecture.components[item.index].elements">' +
+										'	          <b-badge variant="info" ' +
+										'	                   class="border border-info shadow memoryTag" ' +
+										'	                   v-if="item2.properties.includes(\'pointer\') && item2.properties.includes(\'stack\') && ((parseInt(item2.value) & 0xFFFFFFFC) == (row.item.Address & 0xFFFFFFFC))">' +
+										'	            {{item2.name[0]}}' +
+										'	          </b-badge>' +
+										'	          <span class="fas fa-long-arrow-alt-right" ' +
+										'	                v-if="item2.properties.includes(\'pointer\') && item2.properties.includes(\'stack\') && ((parseInt(item2.value) & 0xFFFFFFFC) == (row.item.Address & 0xFFFFFFFC))">' +
+										'	          </span>' +
+										'	' +
+										'	        </div>' +
+										'	      </div>' +
+										'	    </template>' +
+										'	' +
+										'	    <template v-slot:cell(Address)="row">' +
+										'	      <span class="h6Sm text-secondary" v-if="row.item.unallocated==true">' +
+										'	        0x{{((row.item.Address + 3).toString(16)).padStart(row.item.Address.length-2, "0").toUpperCase()}} - 0x{{(row.item.Address.toString(16)).padStart(row.item.Address.length-2, "0").toUpperCase()}}' +
+										'	      </span>' +
+										'	      <span class="h6Sm" v-if="row.item.unallocated==false">' +
+										'	        0x{{((row.item.Address + 3).toString(16)).padStart(row.item.Address.length-2, "0").toUpperCase()}} - 0x{{(row.item.Address.toString(16)).padStart(row.item.Address.length-2, "0").toUpperCase()}}' +
+										'	      </span>' +
+										'	    </template>' +
+										'	' +
+										'	    <template v-slot:cell(Binary)="row">' +
+										'	      <span class="h6Sm text-secondary" v-if="row.item.unallocated==true">' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[3].Tag != null">' +
+										'	          {{row.item.Binary[3].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <span v-if="row.item.Binary[3].Tag == null">' +
+										'	          {{row.item.Binary[3].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[3].Tag != null">' +
+										'	          {{row.item.Binary[3].Tag}}' +
+										'	        </b-badge>' +
+										'	' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[2].Tag != null">' +
+										'	          {{row.item.Binary[2].Bin.toUpperCase()}' +
+										'	        </span> ' +
+										'	        <span v-if="row.item.Binary[2].Tag == null">' +
+										'	          {{row.item.Binary[2].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[1].Tag != null">' +
+										'	          {{row.item.Binary[2].Tag}}' +
+										'	        </b-badge>' +
+										'	' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[1].Tag != null">' +
+										'	          {{row.item.Binary[1].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <span v-if="row.item.Binary[1].Tag == null">' +
+										'	          {{row.item.Binary[1].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[1].Tag != null">' +
+										'	          {{row.item.Binary[1].Tag}}' +
+										'	        </b-badge>' +
+										'	' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[0].Tag != null">' +
+										'	          {{row.item.Binary[0].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <span v-if="row.item.Binary[0].Tag == null">' +
+										'	          {{row.item.Binary[0].Bin.toUpperCase()}}' +
+										'	        </span>' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[0].Tag != null">' +
+										'	          {{row.item.Binary[0].Tag}}' +
+										'	        </b-badge>' +
+										'	      </span>' +
+										'	' +
+										'	      <span class="h6Sm" v-if="row.item.unallocated==false">' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[3].Tag != null">' +
+										'	          {{row.item.Binary[3].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <span v-if="row.item.Binary[3].Tag == null">' +
+										'	          {{row.item.Binary[3].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[3].Tag != null">' +
+										'	          {{row.item.Binary[3].Tag}}' +
+										'	        </b-badge>' +
+										'	' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[2].Tag != null">' +
+										'	          {{row.item.Binary[2].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <span v-if="row.item.Binary[2].Tag == null">' +
+										'	          {{row.item.Binary[2].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[2].Tag != null">' +
+										'	          {{row.item.Binary[2].Tag}}' +
+										'	        </b-badge>' +
+										'	' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[1].Tag != null">' +
+										'	          {{row.item.Binary[1].Bin.toUpperCase()}}' +
+										'	        </span>' +
+										'	        <span v-if="row.item.Binary[1].Tag == null">' +
+										'	          {{row.item.Binary[1].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[1].Tag != null">' +
+										'	          {{row.item.Binary[1].Tag}}' +
+										'	        </b-badge>' +
+										'	' +
+										'	        <span class="memoryBorder" v-if="row.item.Binary[0].Tag != null">' +
+										'	          {{row.item.Binary[0].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <span v-if="row.item.Binary[0].Tag == null">' +
+										'	          {{row.item.Binary[0].Bin.toUpperCase()}}' +
+										'	        </span> ' +
+										'	        <b-badge pill variant="info" ' +
+										'	                 class="border border-info shadow binaryTag" ' +
+										'	                 v-if="row.item.Binary[0].Tag != null">' +
+										'	          {{row.item.Binary[0].Tag}}' +
+										'	        </b-badge>' +
+										'	      </span>' +
+										'	    </template>' +
+										'	    <template v-slot:cell(Value)="row">' +
+										'	      <span class="h6Sm text-secondary" v-if="row.item.unallocated==true">{{row.item.Value}}</span>' +
+										'	      <span class="h6Sm" v-if="row.item.unallocated==false">{{row.item.Value}}</span>' +
+										'	    </template>' +
+										'	  </b-table>' +
+										'	</div>'
+		  
+				}
+
+        Vue.component('table-mem-stack', uielto_memory_stack)
--- components/simulator/creator_uielto_stats.js
+++ components/simulator/creator_uielto_stats.js
@@ -23,124 +23,22 @@
 
         var uielto_stats = {
 
-        /*Import Graph component*/
-		    components: {
-		      						apexchart: VueApexCharts,
-		    },
-
 			  props:      {
 											data_mode:   { type: String, required: true },
 											stats: 			 { type: Array,  required: true },
-											total_stats: { type: Number, required: true },
 											stats_value: { type: Number, required: true },
 										},
-										
-				data: 			function () {
-											return {
-          							/*Stats table fields*/
-									      statsFields: {
-									        type: {
-									          label: 'Type',
-									          sortable: true
-									        },
-									        number_instructions: {
-									          label: 'Number of instructions',
-									          sortable: true
-									        },
-									        percentage: {
-									          label: 'Percentage',
-									          sortable: true
-									        }
-									      },
-
-									      /*Stats Graph configure*/
-									      chartOptions: {
-									        colors:['red', 'blue', 'yellow', 'purple', 'green', 'orange', 'gray', 'pink', 'teal', 'black', 'lime', 'indigo', 'cyan'],
-									        chart: {
-									          id: 'graphic',
-									          type: 'donut',
-									        },
-									        labels: ["Arithmetic integer", "Arithmetic floating point", "Logic", "Transfer between registers", "Memory access", "Comparison", "I/O", "Syscall", "Control", "Function call", "Conditional bifurcation", "Unconditional bifurcation", "Other"],
-									        dataLabels: {
-									          enabled: true
-									        },
-									        donut: {
-									          labels: {
-									            show: true,
-									            total: {
-									              show: true,
-									              showAlways: true,
-									              label: "Total",
-									            },
-									          },
-									        },
-									        fill: {
-									          type: 'gradient',
-									          gradient: {
-									            shade: 'dark',
-									            type: "horizontal",
-									            shadeIntensity: 0.5,
-									            gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
-									            inverseColors: true,
-									            opacityFrom: 1,
-									            opacityTo: 1,
-									            stops: [0, 50, 100],
-									            colorStops: []
-									          },
-									          colors: ['red', 'blue', 'yellow', 'purple', 'green', 'orange', 'gray', 'pink', 'teal', 'black', 'lime', 'indigo', 'cyan'],
-									        },
-									        legend: {
-									          formatter: function(val, opts) {
-									            return val + " - " + opts.w.globals.series[opts.seriesIndex]
-									          }
-									        },
-									        plotOptions: {
-									          pie: {
-									            donut: {
-									              labels: {
-									                show: true,
-									                total: {
-									                  show: true,
-									                  showAlways: true,
-									                  color: 'black',
-									                  formatter: function (w) {
-									                    return w.globals.seriesTotals.reduce((a, b) => {
-									                      return a + b
-									                    }, 0)
-									                  }
-									                }
-									              }
-									            }
-									          }
-									        },
-									      },
-  										}
-        						},
 
       	template:   '	<div class="col-lg-12 col-sm-12" id="stats" v-if="data_mode == \'stats\'">' +
 										'	  <div class="col-lg-12 col-sm-12">' +
 										'	' +
 										'	    <b-tabs content-class="mt-3">' +
 										'	      <b-tab title="Ghaphic" active>' +
-										'	        <div id="chart" class="stats">' +
-										'	          <apexchart id="graphic" ' +
-										'	                     ref="chart" ' +
-										'	                     type="donut" ' +
-										'	                     :options="chartOptions" ' +
-										'	                     :series="stats_value" ' +
-										'	                     height="150%" >' +
-										'	          </apexchart>' +
-										'	        </div>' +
+										'	        <plot-stats :stats_value="stats_value"></plot-stats> ' +
 										'	      </b-tab>' +
 										'	' +
 										'	      <b-tab title="Table">' +
-										'	        <b-table striped ' +
-										'	                 small ' +
-										'	                 hover ' +
-										'	                 :items="stats" ' +
-										'	                 :fields="statsFields" ' +
-										'	                 class="stats text-center">' +
-										'	        </b-table>' +
+										'	        <table-stats :stats="stats"></table-stats>' +
 										'	      </b-tab>' +
 										'	    </b-tabs>' +
 										'	  </div>' +
