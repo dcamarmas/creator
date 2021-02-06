fd629255aa0f1f4717d39288bfa4d3644d11b91d
Diego <diego.camarmas.alonso@gmail.com>
2021-02-06 12:37:49 +0100

    3.0: MIPS-32 & RISC-V architectures updated

--- js/min.creator_node.js
+++ js/min.creator_node.js
@@ -19,21 +19,76 @@
  */
 
 
-  function bi_intToBigInt ( int_value, int_base )
-  {
-       var auxBigInt = null ;
+function bi_intToBigInt ( int_value, int_base )
+{
+	var auxBigInt = null ;
 
-       if (typeof bigInt !== "undefined" && int_base == 16)
-            auxBigInt = bigInt(int_value, int_base).value ;
+	if (typeof bigInt !== "undefined" && int_base == 16)
+		auxBigInt = bigInt(int_value, int_base).value ;
 
-       else if (typeof bigInt !== "undefined")
-            auxBigInt = bigInt(parseInt(int_value) >>> 0, int_base).value ;
+	else if (typeof bigInt !== "undefined")
+		auxBigInt = bigInt(parseInt(int_value) >>> 0, int_base).value ;
 
-       else auxBigInt = BigInt(parseInt(int_value) >>> 0, int_base) ;
+	else auxBigInt = BigInt(parseInt(int_value) >>> 0, int_base) ;
 
-       return auxBigInt ;
-  }
+	return auxBigInt ;
+}
 
+
+/*String to Bigint number*/
+function bigInt_deserialize(object)
+{
+	var auxObject = object;
+
+	for (var i=0; i<auxObject.components.length; i++)
+	{
+		var aux = null ;
+		var auxBigInt = null ;
+
+		if (auxObject.components[i].type != "floating point")
+		{
+			for (var j = 0; j < auxObject.components[i].elements.length; j++)
+			{
+				aux = auxObject.components[i].elements[j].value;
+				auxObject.components[i].elements[j].value = bi_intToBigInt(aux,10) ;
+
+				if (auxObject.components[i].double_precision != true)
+				{
+				  aux = auxObject.components[i].elements[j].default_value;
+				  auxObject.components[i].elements[j].default_value = bi_intToBigInt(aux,10) ;
+				}
+			}
+		}
+	}
+
+	return auxObject;
+}
+
+/*Bigint number to string*/
+function bigInt_serialize(object)
+{
+	var auxObject = jQuery.extend(true, {}, object);
+
+	for (var i=0; i<architecture.components.length; i++)
+	{
+		if (architecture.components[i].type != "floating point")
+	   	{
+		   for (var j = 0; j < architecture.components[i].elements.length; j++)
+			{
+				var aux = architecture.components[i].elements[j].value;
+				auxObject.components[i].elements[j].value = Number(aux);
+
+				if (architecture.components[i].double_precision != true)
+				{
+					var aux = architecture.components[i].elements[j].default_value;
+					auxObject.components[i].elements[j].default_value = Number(aux);
+				}
+		   }
+		}
+	}
+
+	return auxObject;
+}
 /*
  *  Copyright 2018-2021 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos
  *
@@ -649,60 +704,6 @@ var display = '' ;
 // Load architecture
 //
 
-/*String to Bigint number*/
-function bigInt_deserialize(object)
-{
-    var auxObject = object;
-
-    for (var i=0; i<auxObject.components.length; i++)
-    {
-        var aux = null ;
-        var auxBigInt = null ;
-
-        if (auxObject.components[i].type != "floating point")
-        {
-            for (var j = 0; j < auxObject.components[i].elements.length; j++)
-	    {
-                 aux = auxObject.components[i].elements[j].value;
-                 auxObject.components[i].elements[j].value = bi_intToBigInt(aux,10) ;
-
-                 if (auxObject.components[i].double_precision != true)
-	         {
-                     aux = auxObject.components[i].elements[j].default_value;
-                     auxObject.components[i].elements[j].default_value = bi_intToBigInt(aux,10) ;
-                 }
-            }
-        }
-    }
-
-    return auxObject;
-}
-
-/*Bigint number to string*/
-function bigInt_serialize(object)
-{
-    var auxObject = jQuery.extend(true, {}, object);
-
-    for (var i=0; i<architecture.components.length; i++)
-    {
-        if (architecture.components[i].type != "floating point")
-  	   {
-               for (var j = 0; j < architecture.components[i].elements.length; j++)
-	       {
-                    var aux = architecture.components[i].elements[j].value;
-                    auxObject.components[i].elements[j].value = aux.toString();
-
-                    if (architecture.components[i].double_precision != true)
-		    {
-                        var aux = architecture.components[i].elements[j].default_value;
-                        auxObject.components[i].elements[j].default_value = aux.toString();
-                    }
-               }
-           }
-    }
-
-    return auxObject;
-}
 
 // Load architecture
 
