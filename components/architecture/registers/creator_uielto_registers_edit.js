a536bcd18c97a16704ebb0b4bb7130e49b803bb4
Diego <diego.camarmas.alonso@gmail.com>
2021-12-24 12:24:36 +0100

    3.1: Register components added

--- components/architecture/registers/creator_uielto_registers_edit.js
+++ components/architecture/registers/creator_uielto_registers_edit.js
@@ -30,11 +30,11 @@
                       double_precision:               { type: String, required: true },
                       reg_id:                         { type: Number, required: true },
                       name:                           { type: String, required: true },
-                      defValue:                       { type: String, required: true },
+                      def_value:                       { type: String, required: true },
                       properties:                     { type: Array,  required: true },
                       precision:                      { type: String, required: true },
                       simple1:                        { type: String, required: true },
-                      simple2:                        { type: String, required: true }
+                      simple_2:                        { type: String, required: true }
                       
                     },
 
@@ -50,10 +50,10 @@
                       //Check all field of modified register
                       edit_register_verify(evt, comp){
                         evt.preventDefault();
-                        if (this._props.name.length == 0 || !this._props.name || !this._props.defValue) {
+                        if (this._props.name.length == 0 || !this._props.name || !this._props.def_value) {
                           show_notification('Please complete all fields', 'danger') ;
                         }
-                        else if(isNaN(this._props.defValue)){
+                        else if(isNaN(this._props.def_value)){
                           show_notification('The default value must be a number', 'danger') ;
                         }
                         else {
@@ -74,18 +74,18 @@
                           }
                         }
 
-                        this.showEditElement = false;
+                        this.show_modal = false;
 
                         for (var i = 0; i < architecture_hash.length; i++){
                           for(var j=0; j < architecture.components[i].elements.length; j++){
                             if(comp == architecture.components[i].elements[j].name){
                               architecture.components[i].elements[j].name = this._props.name;
                               if(architecture.components[i].type == "control" || architecture.components[i].type == "integer"){
-                                architecture.components[i].elements[j].default_value = bi_intToBigInt(this._props.defValue,10) ;
+                                architecture.components[i].elements[j].default_value = bi_intToBigInt(this._props.def_value,10) ;
                               }
                               else{
                                 if(architecture.components[i].double_precision == false){
-                                  architecture.components[i].elements[j].default_value = parseFloat(this._props.defValue, 10);
+                                  architecture.components[i].elements[j].default_value = parseFloat(this._props.def_value, 10);
                                 }
                                 else{
 
@@ -96,20 +96,20 @@
                                   for (var a = 0; a < architecture_hash.length; a++) {
                                     for (var b = 0; b < architecture.components[a].elements.length; b++) {
                                       if(architecture.components[a].elements[b].name == this._props.simple1){
-                                        aux_sim1 = this.bin2hex(this.float2bin(architecture.components[a].elements[b].value));
+                                        aux_sim1 = bin2hex(float2bin(architecture.components[a].elements[b].value));
                                       }
-                                      if(architecture.components[a].elements[b].name == this._props.simple2){
-                                        aux_sim2 = this.bin2hex(this.float2bin(architecture.components[a].elements[b].value));
+                                      if(architecture.components[a].elements[b].name == this._props.simple_2){
+                                        aux_sim2 = bin2hex(float2bin(architecture.components[a].elements[b].value));
                                       }
                                     }
                                   }
 
                                   aux_value = aux_sim1 + aux_sim2;
 
-                                  architecture.components[i].elements[j].value = this.hex2double("0x" + aux_value);
+                                  architecture.components[i].elements[j].value = hex2double("0x" + aux_value);
 
                                   architecture.components[i].elements[j].simple_reg[0] = this._props.simple1;
-                                  architecture.components[i].elements[j].simple_reg[1] = this._props.simple2;
+                                  architecture.components[i].elements[j].simple_reg[1] = this._props.simple_2;
                                 }
                               }
                               architecture.components[i].elements[j].properties = this._props.properties;
@@ -131,6 +131,23 @@
                         else{
                           return true;
                         }
+                      },
+
+                      //Convert floating point number to binary
+                      float2bin (number)
+                      {
+                          return float2bin(number) ;
+                      },
+
+                      //Convert binary number to hexadecimal number
+                      bin2hex(s)
+                      {
+                          return bin2hex(s) ;
+                      },
+
+                      //Convert hexadecimal number to double floating point number
+                      hex2double ( hexvalue ){
+                          return hex2double(hexvalue) ;
                       },
 
                       //Stop user interface refresh
@@ -170,8 +187,7 @@
                     '         title="Edit register" ' +
                     '         ok-title="Save"' +
                     '         @ok="edit_register_verify($event, element)" ' +
-                    '         v-model="show_modal" ' +
-                    '         @hidden="emptyFormArch">' +
+                    '         v-model="show_modal">' +
                     '  <b-form>' +
                     '    <b-form-group label="Name:">' +
                     '      <b-form-tags' +
@@ -200,17 +216,17 @@
                     '    <b-form-group label="Default value:" ' +
                     '                  v-if="double_precision == false">' +
                     '      <b-form-input type="text" ' +
-                    '                    v-on:input="debounce(\'defValue\', $event)" ' +
-                    '                    :value="defValue" ' +
+                    '                    v-on:input="debounce(\'def_value\', $event)" ' +
+                    '                    :value="def_value" ' +
                     '                    required ' +
                     '                    placeholder="Enter default value" ' +
-                    '                    :state="valid(defValue)" ' +
+                    '                    :state="valid(def_value)" ' +
                     '                    size="sm" ' +
                     '                    title="Default value">' +
                     '      </b-form-input>' +
                     '    </b-form-group>' +
-                    '    <b-form-group label="Properties:">' +
-                    '      <b-form-checkbox-group v-model="properties" v-if="type != \'control\'">' +
+                    '    <b-form-group label="Properties:" v-if="type != \'control\'">' +
+                    '      <b-form-checkbox-group v-model="properties">' +
                     '        <b-form-checkbox value="read">Read</b-form-checkbox>' +
                     '        <b-form-checkbox value="write">Write</b-form-checkbox>' +
                     '        <b-form-checkbox value="ignore_write">Ignore Write</b-form-checkbox>' +
@@ -235,8 +251,8 @@
                     '        </div>' +
                     '        <div class="col-lg-6 col-sm-6">' +
                     '          <b-form-select required ' +
-                    '                         v-model=".simple2" ' +
-                    '                         :state="valid(.simple2)" ' +
+                    '                         v-model="simple_2" ' +
+                    '                         :state="valid(simple_2)" ' +
                     '                         size="sm" ' +
                     '                         :options="simple_reg"' +
                     '                         title="Second part of double">' +
@@ -245,7 +261,7 @@
                     '      </div>' +
                     '    </b-form-group>' +
                     '  </b-form>' +
-                    '</b-modal >'
+                    '</b-modal>'
 
   }
 
