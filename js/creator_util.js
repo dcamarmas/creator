2799c50012775a803d5fd3ce78c8a445ecbbcef9
dcamarmas <dcamarma@inf.uc3m.es>
2022-09-05 22:08:45 +0200

    3.1: register minor update

--- js/creator_util.js
+++ js/creator_util.js
@@ -175,11 +175,11 @@
 
       for (i = 0; i < 8; i++)
       {
-          var bits = dv.getUint8(i).toString(2);
-          if (bits.length < 8) {
-            bits = new Array(8 - bits.length).fill('0').join("") + bits;
-          }
-          result += bits;
+        var bits = dv.getUint8(i).toString(2);
+        if (bits.length < 8) {
+          bits = new Array(8 - bits.length).fill('0').join("") + bits;
+        }
+        result += bits;
       }
       return result;
   }
