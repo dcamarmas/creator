da117d5731b77a26bfd2e8e3bdd9356aef94ee94
dcamarmas <diego.camarmas.alonso@gmail.com>
2022-09-15 19:47:14 +0200

    3.1: travis test added

---
+++ travis/mips/instructions/test-14.s
@@ -0,0 +1,26 @@
+
+#
+# Creator (https://creatorsim.github.io/creator/)
+#
+
+.data
+    .align 2
+    b1:		.byte 127
+    h1:		.half 65535
+    b2:		.space 1
+    h2:		.space 2
+
+.text
+main:
+
+    la $t0 b1
+    la $t1 h1
+
+    lbu $t2 0($t0)
+    lhu $t3 0 ($t1)
+
+    la $t4 b2
+    sb $t5 0($t4)
+
+    la $t6 h2
+    sh $t7 0($t6)
