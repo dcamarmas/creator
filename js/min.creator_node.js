e0bcc59dc20fcbd0ef28a2c247e57e6ce9a0219f
dcamarmas <diego.camarmas.alonso@gmail.com>
2023-01-09 17:37:41 +0100

    Google Analytics

--- js/min.creator_node.js
+++ js/min.creator_node.js
@@ -146,7 +146,7 @@ function register_value_serialize( architecture )
   return aux_architecture;
 }
 /*
- *  Copyright 2018-2022 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos
+ *  Copyright 2018-2023 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos
  *
  *  This file is part of CREATOR.
  *
@@ -174,18 +174,26 @@ function register_value_serialize( architecture )
 
   function creator_ga ( category, action, label )
   {
-      if (typeof ga === "undefined") {
-          return ;
-      }
-
+    if (typeof ga !== "undefined") {
       if (is_ga_initialize == false)
       {
-          ga('create', 'UA-186823627-2', 'auto') ;
-          ga('set', 'transport', 'beacon') ;
-          is_ga_initialize = true ;
+        ga('create', 'UA-186823627-2', 'auto') ;
+        ga('set', 'transport', 'beacon') ;
+        is_ga_initialize = true ;
       }
 
       ga('send', 'event', category, action, label) ;
+    }
+
+    if (typeof gtag !== "undefined") {
+
+      gtag('event',
+            action,
+            {
+              'event_category' : "creator_"+category,
+              'event_label' : label
+            });
+    }
   }
 
 /*
