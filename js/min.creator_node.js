f3520fe348cac3d9f1403e61c82f9cd01e0b266e
dcamarmas <diego.camarmas.alonso@gmail.com>
2023-03-15 19:16:31 +0100

    3.2.1: Google Analytics event minor update

--- js/min.creator_node.js
+++ js/min.creator_node.js
@@ -174,9 +174,10 @@ function register_value_serialize( architecture )
 
     if (typeof gtag !== "undefined") {
       gtag('event',
-            action,
+            label,
             {
               'event_category' : "creator_"+category,
+              'event_action' : action,
               'event_label' : label
             });
     }
