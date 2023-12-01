081a15fb08734ba085553a08bf4c599acd1e3408
Diego Camarmas <dcamarma@inf.uc3m.es>
2023-12-01 18:15:09 +0100

    4.1: gateway docker minor update

--- dockers/gateway/container_start.sh
+++ dockers/gateway/container_start.sh
@@ -1,14 +1,70 @@
 #!/bin/bash
-set -x
 
-if [ "$#" -eq 0 ]; then
-	TARGET_BOARD=esp32c3
-else
-	TARGET_BOARD=$1
-fi
+
+#
+# Default values
+#
+TARGET_BOARD=esp32c3
+TARGET_PORT=/dev/ttyUSB0
+
+
+#
+# Auxiliar functions
+#
+
+intro() {
+   echo ""
+   echo " CREATOR Gateway"
+   echo " ---------------"
+   echo ""
+}
+
+usage() {
+   echo " Usage: $0 [--target_board      esp32c3] \\"
+   echo "                             [--target_port       /dev/ttyUSB0] \\"
+   echo ""
+}
+
+info() {
+   echo " * Target Board: "${TARGET_BOARD}
+   echo " * Target Port:  "${TARGET_PORT}
+   echo ""
+}
+
+get_opts() {
+   # Taken the general idea from https://stackoverflow.com/questions/70951038/how-to-use-getopt-long-option-in-bash-script
+   name=$(basename "$0")
+   short_opt=b:,p:,h
+   long_opt=target_board:,target_port:,help
+   TEMP=$(getopt -o $short_opt --long $long_opt --name "$name" -- "$@")
+   eval set -- "${TEMP}"
+
+   while :; do
+      case "${1}" in
+         -b | --target_board     ) TARGET_BOARD=$2;         shift 2 ;;
+         -p | --target_port      ) TARGET_PORT=$2;          shift 2 ;;
+         -h | --help             ) intro; usage;  exit 0 ;;
+         --                      ) shift;         break  ;;
+         *                       ) intro; echo " > ERROR: parsing arguments found an error :-/"; usage; exit 1 ;;
+      esac
+   done
+}
+
+
+#
+# Main
+#
+
+# Check arguments, and print it
+get_opts $@
+intro
+check_opts
+info
 
 docker container stop $(docker container ls -q --filter name=creator_gateway)
 docker container rm creator_gateway
 
 docker build -t creator_gateway . --build-arg TARGET_BOARD=${TARGET_BOARD}
-docker run --init -it -v /dev/:/dev/ -p 8080:8080 --name creator_gateway creator_gateway
+docker run --init -it --device=${TARGET_PORT} -p 8080:8080 --name creator_gateway creator_gateway
+
+echo " Done."
