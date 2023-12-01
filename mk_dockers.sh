d910ff95ba69830479b631c8831add653cfbdb23
Diego Camarmas <dcamarma@inf.uc3m.es>
2023-12-01 17:19:58 +0100

    4.1: comman line docker added

--- mk_dockers.sh
+++ mk_dockers.sh
@@ -13,11 +13,21 @@ echo "  * Gateway docker..."
 echo "  * Command line docker..."
 echo ""
 
-cd dockers/gateway
+cd dockers
 
+#Build gateway docker
+cd gateway
 docker container stop $(docker container ls -q --filter name=creator_gateway)
 docker container rm creator_gateway
 docker build -t creator_gateway .
+cd ..
+
+#Build command line docker
+cd command_line
+docker container stop $(docker container ls -q --filter name=creator_cl)
+docker container rm creator_cl
+docker build -t creator_cl .
+cd ..
 
 
 
