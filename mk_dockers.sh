248e55a410d9d01f58a308b3fff2d789066c558b
Diego Camarmas <dcamarma@inf.uc3m.es>
2023-12-02 13:15:22 +0100

    4.1: dockers minor updates

--- mk_dockers.sh
+++ mk_dockers.sh
@@ -17,16 +17,16 @@ cd dockers
 
 #Build gateway docker
 cd gateway
-docker container stop $(docker container ls -q --filter name=creator_gateway)
-docker container rm creator_gateway
-docker build -t creator_gateway .
+docker container stop $(docker container ls -q --filter name=creatorsim/creator_gateway)
+docker container rm creatorsim/creator_gateway
+docker build -t creatorsim/creator_gateway .
 cd ..
 
 #Build command line docker
 cd command_line
-docker container stop $(docker container ls -q --filter name=creator_cl)
-docker container rm creator_cl
-docker build -t creator_cl .
+docker container stop $(docker container ls -q --filter name=creatorsim/creator_cl)
+docker container rm creatorsim/creator_cl
+docker build -t creatorsim/creator_cl .
 cd ..
 
 
