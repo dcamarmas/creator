14cdba30c6a995aaca0cdb2fa0abee9e001ae427
Diego Camarmas <dcamarma@inf.uc3m.es>
2024-02-14 11:51:24 +0100

    4.1: hw_lab minor updates

--- dockers/hw_lab/start_hw_lab.sh
+++ dockers/hw_lab/start_hw_lab.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#Load espressif environment
-export IDF_PATH=/esp/esp-idf/
-. /esp/esp-idf/export.sh
-
 #Run gateway
 export PYTHONPATH=/usr/local/lib/python3.10/dist-packages:${PYTHONPATH}
-cd /drivers
-python3 gateway.py
+cd /creator/hw_lab/
+python3 hw_lab.py deployment.json
