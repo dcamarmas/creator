c1d27bb2db8dd78e43263f6c4fe36648b99b8ae8
Diego Camarmas <dcamarma@inf.uc3m.es>
2024-10-04 10:05:48 +0200

    4.1: architecture editor minor update

---
+++ components/architecture/creator_uielto_edit_architecture.js
@@ -0,0 +1,60 @@
+
+/*
+ *  Copyright 2018-2024 Felix Garcia Carballeira, Diego Camarmas Alonso, Alejandro Calderon Mateos
+ *
+ *  This file is part of CREATOR.
+ *
+ *  CREATOR is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CREATOR is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with CREATOR.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+  /* jshint esversion: 6 */
+
+  var uielto_edit_architecture = {
+
+    props:      {
+                  id:                  { type: String, required: true }
+                },
+
+    data:       function () {
+                  return {
+
+                  }
+                },
+
+    methods:    {
+                  //Save edit architecture
+                  arch_edit_save()
+                  {
+
+                  },
+
+                  arch_edit_codemirror_save()
+                  {
+
+                  }
+                },
+
+    template:   '<b-modal  :id ="id" ' +
+                '          size="xl" ' +
+                '          title = "Edit Architecture" ' +
+                '          ok-title="Save" ' +
+                '          @ok="arch_edit_save"' +
+                '          @hidden="arch_edit_codemirror_save">' +
+
+                '</b-modal>'
+  }
+
+  Vue.component('edit-architecture', uielto_edit_architecture) ;
