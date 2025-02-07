8a970e1b8ab54b6090f78fd5ecb98da52355f7e8
Luis Daniel Casais <lcasais@pa.uc3m.es>
2025-02-07 17:44:10 +0100

    Initial commit
    Basic Vue template project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { foo, baz } from './stuff/mystuff.mjs'
+import { bar } from './stuff/mycoolstuff.mts'
+
+export default {
+  name: 'app',
+  data() {
+    return { baz: baz }
+  },
+  methods: {
+    call() {
+      return foo() + bar()
+    },
+  },
+}
+</script>
+
+<template>
+  <p>{{ call() + baz }}</p>
+  <div></div>
+</template>
---
+++ src/stuff/mystuff.mjs
@@ -0,0 +1,5 @@
+export function foo() {
+  return 'foo'
+}
+
+export const baz = 'baz'
