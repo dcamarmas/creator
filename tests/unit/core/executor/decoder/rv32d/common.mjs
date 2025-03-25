233947f7ecba5d0e50bc50a5283abfb179a7b564
Jorge Ramos <jorramos@pa.uc3m.es>
2025-03-25 13:28:55 +0100

    Started working on RV32D. Added decoder unit tests. Added RV32D to the new architecture format.

---
+++ tests/unit/core/executor/decoder/rv32d/common.mjs
@@ -0,0 +1,18 @@
+import { newArchitectureLoad } from "../../../../../../src/core/core.mjs";
+import fs from "node:fs";
+import { assertEquals } from "https://deno.land/std/assert/mod.ts";
+import { decode_instruction } from "../../../../../../src/core/executor/decoder.mjs";
+
+export function setupArchitecture(
+    architecturePath = "./architecture/RISCV/RV32IMFD.yml",
+) {
+    const architectureFile = fs.readFileSync(architecturePath, "utf8");
+    newArchitectureLoad(architectureFile, true);
+}
+
+export function decode_test(instruction, expected) {
+    setupArchitecture();
+    const result = decode_instruction(instruction, true);
+
+    assertEquals(result, expected);
+}
