d4457a72605aafe331d5856a5fb61944032c5802
Luis Daniel Casais <lcasais@pa.uc3m.es>
2025-07-22 12:48:46 +0200

    chore(web): fix build
    Bundle `architecture/` and `examples/` folders in dist.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import Components from "unplugin-vue-components/vite";
 import { BootstrapVueNextResolver } from "bootstrap-vue-next";
 import wasm from "vite-plugin-wasm";
+import { DynamicPublicDirectory } from "vite-multiple-assets";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -29,19 +30,39 @@ export default defineConfig({
       resolvers: [BootstrapVueNextResolver()],
     }),
     wasm(),
+
+    // bundle assets with the build, that is, if we have /public/foo.png, it is
+    // served in /foo.png
+    DynamicPublicDirectory([
+      "public/**", // bundle all files inside this folder
+      // bundle these folders
+      {
+        input: "architecture/**",
+        output: "/architecture",
+      },
+      {
+        input: "examples/**",
+        output: "/examples",
+      },
+    ]) as PluginOption,
   ],
+
+  publicDir: false, // we use a DynamicPublicDirectory
+
   resolve: {
+    // aliases for common folders
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "#": fileURLToPath(new URL(".", import.meta.url)),
     },
   },
+
   build: {
     outDir: "dist/web",
     emptyOutDir: true,
-    target: "esnext", //browsers can handle the latest ES features
-    // set codemirror output as a separate file
+    target: "esnext", // browsers can handle the latest ES features
   },
+
   // Silence Sass deprecation warnings (due to bootstrap-vue-next)
   css: {
     preprocessorOptions: {
