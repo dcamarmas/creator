fecd8642a11dbd3e25467ac631fb9dffaece5b22
EUtrilla2002 <100451242@alumnos.uc3m.es>
2025-09-26 10:29:26 +0200

    Changed esp32c2 (no JTAG) for ESP32c6 (proven, working and with JTAG)

--- components/simulator/creator_uielto_target_flash.js
+++ components/simulator/creator_uielto_target_flash.js
@@ -43,7 +43,7 @@
 
                         remote_target_boards =  [
                                                   { text: 'Please select an option', value: "", disabled: true },
-                                                  { text: 'ESP32-C2 (RISC-V)',       value: 'esp32c2' },
+                                                  { text: 'ESP32-C6 (RISC-V)',       value: 'esp32c6' },
                                                   { text: 'ESP32-C3 (RISC-V)',       value: 'esp32c3' },
                                                   { text: 'ESP32-H2 (RISC-V)',       value: 'esp32h2' },
                                                 //{ text: 'ESP32-S2 (MIPS-32)',      value: 'esp32s2' },
@@ -68,7 +68,7 @@
 
                         target_boards = [
                                           { text: 'Please select an option', value: "", disabled: true },
-                                          { text: 'ESP32-C2 (RISC-V)',       value: 'esp32c2' },
+                                          { text: 'ESP32-C6 (RISC-V)',       value: 'esp32c6' },
                                           { text: 'ESP32-C3 (RISC-V)',       value: 'esp32c3' },
                                           { text: 'ESP32-H2 (RISC-V)',       value: 'esp32h2' },
                                         //{ text: 'ESP32-S2 (MIPS-32)',      value: 'esp32s2' },
