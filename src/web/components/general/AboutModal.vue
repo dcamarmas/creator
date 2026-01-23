8e024cc3fffb709e19ff805f4e92200ca147e605
Diego Camarmas <dcamarma@inf.uc3m.es>
2026-01-23 20:38:55 +0100

    Version Updated

--- src/web/components/general/AboutModal.vue
+++ src/web/components/general/AboutModal.vue
@@ -29,7 +29,7 @@ interface Props {
 defineProps<Props>();
 
 const contactMail = "creator.arcos.inf.uc3m.es@gmail.com";
-const projectVersion = "6.0";
+const projectVersion = "6.1.0";
 const projectLicense = "LGPL-2.1";
 const repositoryUrl = "https://github.com/creatorsim/creator";
 
